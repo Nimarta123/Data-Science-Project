0998eddb00853af8ec0e89242d122e0cdb927634
Nimarta123 <140687219+Nimarta123@users.noreply.github.com>
2025-04-29 10:49:26 +0500

    Add files via upload

---
+++ Task02_unemploymentin India.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv('C:\\Users\\Venish\\Documents\\Internship CodeAlpha\\Unemployment in India.csv')
+
+print(df.head())
+print("\nData Summary:\n", df.info())
+print("\nMissing Values:\n", df.isnull().sum())
+
+df.columns = df.columns.str.strip()
+
+plt.figure(figsize=(12,6))
+sns.barplot(x='Region', y='Estimated Unemployment Rate (%)', data=df)
+plt.xticks(rotation=90)
+plt.title('Unemployment Rate by Region')
+plt.show()
+
+df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y', errors='coerce')
+plt.figure(figsize=(14,6))
+sns.lineplot(x='Date', y='Estimated Unemployment Rate (%)', hue='Region', data=df)
+plt.title('Unemployment Rate Over Time by Region')
+plt.show()
